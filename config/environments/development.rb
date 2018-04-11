195744de737a3a7c9d1b474f5c8944af0f5496f7
Huy Tran <huytp.uit@gmail.com>
2018-04-11 09:07:51 +0700

    Change image on login & signup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
+    s3_protocol: :https,
     path: "development/kyc/:attachment/:id_partition/:style/:basename.:extension",
     bucket: ENV["aws_s3_bucket"],
     :s3_credentials => {access_key_id: ENV["aws_access_key_id"], secret_access_key: ENV["aws_secret_access_key"]}
