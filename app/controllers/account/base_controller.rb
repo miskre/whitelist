66c79906b6c37d59c92cde4b00551252213aafd9
Huy Tran <huytp.uit@gmail.com>
2018-04-18 11:34:36 +0700

    Skip before verify_authenticity_token

--- app/controllers/account/base_controller.rb
+++ app/controllers/account/base_controller.rb
@@ -1,6 +1,6 @@
 class Account::BaseController < ActionController::Base
   protect_from_forgery with: :exception
-
+  skip_before_filter :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
   layout 'account/layouts/application'
 
   include Account::SessionsHelper
