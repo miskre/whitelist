f21bd71c392db709e6483f5b32f19d46e48e3783
Huy Tran <huytp.uit@gmail.com>
2018-04-20 11:07:14 +0700

    Add key to ENV

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = '6LePclQUAAAAANrijt0PIQQ_2OVEhBAYP0bEWkXf'
-  config.secret_key = '6LePclQUAAAAAGZ89K1PhciLsn6Q0iVJJwrKNDwV'
+  config.site_key  = ENV['SITE_KEY']
+  config.secret_key = ENV['SECRET_KEY']
 end
