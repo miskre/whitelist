0942bb6d654984a4aa98e2ab538d830591057c7e
Huy Tran <huytp.uit@gmail.com>
2018-05-01 13:55:11 +0700

    Update channel link

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,1 +1,6 @@
 AMOUNT_RANGE = ["Smaller than 1 BTC", "1 to 3 BTC", "Bigger than 3 BTC"]
+TWITTER_LINK = "https://twitter.com/Miskre_Global"
+MEDIUM_LINK = "https://medium.com/miskre"
+REDDIT_LINK = "https://www.reddit.com/r/miskre/"
+TELEGRAM_LINK = "https://t.me/miskre"
+SLACK_LINK = "http://chat.miskre.org/"
